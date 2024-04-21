dc6321755561064cf5963cfd8267682fc66d6b9d
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-04-21 18:32:48 +0530

    Added sorting folders based on date and time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from datetime import datetime
 
 # Replace "C:\Users\vikra\OneDrive\Desktop" with your own Absolute File Path
 desktop_path = os.path.expanduser(r"C:\Users\vikra\OneDrive\Desktop")
@@ -20,7 +21,7 @@ for folder_name in folders:
     folder_path = os.path.join(desktop_path, folder_name)
     os.makedirs(folder_path, exist_ok=True)
 
-# Move files to respective folders
+# Move files to respective folders based on file type
 for file in os.listdir(desktop_path):
     if os.path.isfile(os.path.join(desktop_path, file)):
         file_ext = os.path.splitext(file)[1].lower()
@@ -30,3 +31,18 @@ for file in os.listdir(desktop_path):
                 dest = os.path.join(desktop_path, folder_name, file)
                 shutil.move(src, dest)
                 break
+
+# Create a folder for sorted files
+sorted_folder = os.path.join(desktop_path, "Sorted Files")
+os.makedirs(sorted_folder, exist_ok=True)
+
+# Sort files into folders by modification date
+for file in os.listdir(desktop_path):
+    if os.path.isfile(os.path.join(desktop_path, file)):
+        file_path = os.path.join(desktop_path, file)
+        mod_time = os.path.getmtime(file_path)
+        mod_date = datetime.fromtimestamp(mod_time).strftime("%Y-%m-%d")
+        dest_folder = os.path.join(sorted_folder, mod_date)
+        os.makedirs(dest_folder, exist_ok=True)
+        dest_file = os.path.join(dest_folder, file)
+        shutil.move(file_path, dest_file)
